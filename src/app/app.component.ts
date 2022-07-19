e647b8745290cb6ed9d3a7f087dce46c4dc2c300
elcordova <erick.lenin.03@gmail.com>
2022-07-19 13:38:24 -0500

    calculator tests and globlal check coverage config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Calculator } from './calculator';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const calculator = new Calculator();
+    const result = calculator.multiply(2, 3);
+
+  }
   title = 'ng-testig';
 }
